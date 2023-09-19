3d720a43235c89075717ffc6fe7dc4889fe62b91
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-09-20 00:13:18 +0400

    added foldrs: controllers & services & entity

--- server.go
+++ server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/GiorgiMakharadze/video-service-API-golang/controller"
+	"github.com/GiorgiMakharadze/video-service-API-golang/service"
 	"github.com/gin-gonic/gin"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
 )
 
 var(
@@ -15,10 +15,13 @@ var(
 func main() {
 	server := gin.Default()
 
-	server.GET("/posts", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "OK!!",
-		})
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
 	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
 	server.Run(":8080")
 }
